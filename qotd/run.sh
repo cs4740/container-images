2503178b5cab3265deb7a8a4c6b503a6f65014db
Neal Magee <nem2p@virginia.edu>
2021-08-09 17:04:32 -0400

    Better script

--- qotd/run.sh
+++ qotd/run.sh
@@ -4,6 +4,10 @@ set -e
 
 echo "";
 
-curl -s "https://quotes.rest/qod?language=en" | jq -r .contents.quotes[0].quote || exit 1;
+QUOTE=`curl -s "https://quotes.rest/qod?language=en" | jq -r .contents.quotes[0].quote || exit 1;`
+AUTHOR=`curl -s "https://quotes.rest/qod?language=en" | jq -r .contents.quotes[0].author || exit 1;`
+
+echo "  $QUOTE";
+echo "      -$AUTHOR";
 
 echo "";
