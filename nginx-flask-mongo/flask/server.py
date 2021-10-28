abce34dc0eed9a70f83a3dc27aaaa39149f5c475
Neal Magee <nem2p@virginia.edu>
2021-10-28 12:29:49 -0400

    Cleanup

--- nginx-flask-mongo/flask/server.py
+++ nginx-flask-mongo/flask/server.py
@@ -16,7 +16,6 @@ def todo():
         return "Server not available"
     return "Hello from the MongoDB client!\n"
 
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
 
